4198d23df4cfb280928695d5ec99a3f74b97fad6
larry-cm <larryycel@gmail.com>
2025-09-24 18:21:07 -0500

    quitando la config para correr solo en modo desarrollo

--- app.py
+++ app.py
@@ -68,8 +68,8 @@ def serve_frontend(path):
     else:
         return send_from_directory(app.static_folder, 'index.html')
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # Crear las tablas en la base de datos antes de la primera solicitud
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+with app.app_context():
+    db.create_all()
+app.run(debug=True, host="0.0.0.0", port=5000)
